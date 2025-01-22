c5407bb1b6284436126330fbd8f6e07296851df0
Michael Spari <michaelspari72@gmail.com>
2025-01-22 07:46:36 +0100

    center styles

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AboutTheFoldComponent } from './about-the-fold/about-the-fold.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from "../shared/footer/footer.component";
 
 @Component({
   selector: 'app-main-content',
@@ -10,7 +12,9 @@ import { MySkillsComponent } from './my-skills/my-skills.component';
     AboutTheFoldComponent,
     AboutMeComponent,
     MySkillsComponent,
-  ],
+    ProjectsComponent,
+    FooterComponent
+],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 
