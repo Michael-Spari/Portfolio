5e60647c56fea3296232744d2ce5ec426375e7be
Michael Spari <michaelspari72@gmail.com>
2025-01-26 15:04:41 +0100

    test upload michaelspari.de

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'michaelspari72@gmail.com';  
+            $recipient = 'info@michaelspari.de';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
