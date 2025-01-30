db9cacb2f0001847e23de3df6386af2c976e561c
Michael Spari <michaelspari72@gmail.com>
2025-01-30 18:13:03 +0100

    responsiv

--- src/app/main-content/my-skills/my-skills.component.ts
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -16,4 +16,22 @@ export class MySkillsComponent {
   toggleImage() {
     this.showFirstImage = !this.showFirstImage;
   }
+
+  skills = [
+    { name: 'HTML', icon: 'assets/icon/skills/html.png' },
+    { name: 'CSS', icon: 'assets/icon/skills/css.png' },
+    { name: 'JavaScript', icon: 'assets/icon/skills/js.png' },
+    { name: 'TypeScript', icon: 'assets/icon/skills/ts.png' },
+    { name: 'Angular', icon: 'assets/icon/skills/angular.png' },
+    { name: 'Firebase', icon: 'assets/icon/skills/firebase.png' },
+    { name: 'Git', icon: 'assets/icon/skills/git.png' },
+    { name: 'Rest-Api', icon: 'assets/icon/skills/api.png' },
+    { name: 'Flutter', icon: 'assets/icon/skills/flutter.png' },
+    { name: 'Dart', icon: 'assets/icon/skills/dart.png' },
+    { name: 'Pinecone', icon: 'assets/icon/skills/pinecone.png' },
+    { name: 'Photoshop', icon: 'assets/icon/skills/photoshop.png' },
+    { name: 'Illustrator', icon: 'assets/icon/skills/illustrator.png' },
+    { name: 'Scrum', icon: 'assets/icon/skills/scrum.png' },
+    { name: 'MDesign', icon: 'assets/icon/skills/materialDesign.png' }
+  ];
 }
