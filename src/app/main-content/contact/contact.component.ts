17f463b9934c7c3666cf1aca061c3483bdca0183
Michael Spari <michaelspari72@gmail.com>
2025-03-17 11:51:53 +0100

    final corrections

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -4,11 +4,11 @@ import { FooterComponent } from '../../shared/footer/footer.component';
 import { FormsModule, NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import {TranslatePipe} from "@ngx-translate/core";
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FooterComponent, FormsModule, TranslatePipe],
+  imports: [CommonModule, FooterComponent, FormsModule, TranslatePipe, RouterLink],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
